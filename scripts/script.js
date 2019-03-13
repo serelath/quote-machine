573c2cfa423614d46e8fd3aba67cd4c9241482d4
serelath <shoh@go.pasadena.edu>
2019-03-12 19:51:57 -0700

    Twitter share quote

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,28 @@
 var text = document.getElementById("text");
 var author = document.getElementById("author");
 var button = document.getElementById("new-quote");
+var tweetQuote = document.getElementById("tweet-quote");
 
 var quotes = {
     "1": {
-        text: "quote#1",
-        author: "author#1",
+        text: "quote-1",
+        author: "author-1",
     },
     "2": {
-        text: "quote#2",
-        author: "author#2",
+        text: "quote-2",
+        author: "author-2",
+    },
+    "3": {
+        text: "quote-3",
+        author: "author-3",
+    },
+    "4": {
+        text: "quote-4",
+        author: "author-4",
+    },
+    "5": {
+        text: "quote-5",
+        author: "author-5",
     },
 };
 
@@ -21,6 +34,7 @@ function newQuote() {
     
     text.innerHTML = quotes[mathRandom].text;
     author.innerHTML = quotes[mathRandom].author;
+    tweetQuote.href="https://twitter.com/intent/tweet/?text=" +  quotes[mathRandom].text + quotes[mathRandom].author;
     
 }
 
